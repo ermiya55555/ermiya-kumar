f98bf5d6e0753ef0a3f6e5626d978417cfc62f5a
ermiya55555 <162983484+ermiya55555@users.noreply.github.com>
2024-03-11 16:29:51 +0530

    Add files via upload

--- file_py1.py
+++ file_py1.py
@@ -1,4 +1,4 @@
-day one mornig
+(day one mornig)9:00/12:00 (11/03/2024)
 
 print("hellow world")
 1.commenting is used for making docmentation in a python file
@@ -15,11 +15,77 @@ print("hello")
 """
 print("hello")
 '''
-
 inport this
- 
 # import is a keyword which is used to communicated between the python files
 
-second afternoon
-bargained characters
--->
+
+(second afternoon)1:00/5:00
+
+BARGAINED CHARACTERS
+1.controls and data strutures--->abc language
+2.string and its formats--->from c
+3.regular expression--->pearl
+4.exception handling--->module 3
+5.magic methods from--->fortron
+
+ WHAT KIND OF LANGUAGE IS PYTHON
+ *object oriented programming
+ *dynamicallly typed language
+ *scripting language (not neccsary)
+ *interpreted language( n n )
+ *compiled language
+ *functional programming language
+ 
+ FLAVOURS OF PYTHON
+ *Cpython-Written in c,most common implementation of python
+ *Jython- java implementation of python
+ *Ironpython-implementation of the python programming language which is tightly integrated with NET
+ *Brython-browser python runs in browsers
+ *Rubypython-bridge between python and rudy interpreters
+ *Micropython-runs in micro controllers
+
+#print(object,sep,end,file,flush)this are the parameters
+*object--->the value to be printed is object
+*sep--->seperator
+eg:
+a=8
+b=9
+c=8print(a,b,c, sep='$'
+
+*end--->which is used to add the string at the end of the value,
+it will not keep the command to stay in same line 
+eg:
+print(str(a),b, end='%%')
+print(str(c))
+
+###  !---> VARIABLES
+ ?variables is a name which is used to identify the value of variable
+ ?which is stored in a specific memory location
+ 
+RULES TO DEDFINE VARIABLES
+1.variable have to start with only alphabets underscores
+2.the char following the 1st char can be alphabets , numbers,underscores
+3.no spaces are allowed
+4.no special characters like !@#$%^&*()+
+5.case sensitive 6.keywords are not allowed as variables
+
+! WAY TO DEFINE VARIABLES
+a=8,7,9.89, "hello"  (inculed the int,str,fola)
+print(a)
+print(type(a)) this form of tuple
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
