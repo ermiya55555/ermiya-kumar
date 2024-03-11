b8efc01237964c641193e4e7f504c939984991ec
ermiya55555 <162983484+ermiya55555@users.noreply.github.com>
2024-03-11 16:18:44 +0530

    file

---
+++ file_py1.py
@@ -0,0 +1,25 @@
+day one mornig
+
+print("hellow world")
+1.commenting is used for making docmentation in a python file
+2.when the line is commented, that line will not considered as a part of the code
+3.when the progarm is executed commented line will not be in the part of excution
+---->note;
+singel line commenting can be done using # at the begining of the line
+eg;
+   #print("hello")
+
+multiple line commenting can be done using '''code'''. the lines between the quotes will be commented
+eg;
+print("hello")
+"""
+print("hello")
+'''
+
+inport this
+ 
+# import is a keyword which is used to communicated between the python files
+
+second afternoon
+bargained characters
+-->
